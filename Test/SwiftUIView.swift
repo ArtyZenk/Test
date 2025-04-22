a33938a2e048419b03b294f059a37e8c3c43b28c
Artyom Guzenko <artyomka13@gmail.com>
2025-04-22 14:16:51 +0300

    dev:new file

---
+++ Test/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Test
+//
+//  Created by Artyom Guzenko on 22.04.2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
